b44d05b9549ea72a83d3898179b7f9fafd1d18d4
husni habil <habilhusni@gmail.com>
2020-02-09 07:27:21 +0700

    Bump mongoose to >=5.7.5

--- models/pet.js
+++ models/pet.js
@@ -1,14 +1,11 @@
-const mongoose  = require('mongoose'),
-      Schema    = mongoose.Schema,
+const mongoose = require("mongoose"),
+  Schema = mongoose.Schema,
+  petSchema = new Schema({
+    Id: { type: Number, required: true, unique: true },
+    Name: { type: String, required: true },
+    Age: { type: Number, required: true },
+    Photo: { type: String, required: true }
+  }),
+  Pet = mongoose.model("Pet", petSchema);
 
-      petSchema = new Schema({
-        Id    : {type: Number, required: true, unique: true},
-        Name  : {type: String, required: true},
-        Age   : {type: Number, required: true},
-        Photo : {type: String, required: true},
-      }),
-
-      Pet       = mongoose.model('Pet', petSchema);
-
-
-module.exports  = Pet;
+module.exports = Pet;
