36496c50f80aacb15c863f5f61b2c9b833495d2e
husni habil <habilhusni@gmail.com>
2020-06-29 14:33:40 +0700

    Added new status 401 for authentication

--- autentikasi/auth.js
+++ autentikasi/auth.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 var jwt = require("jsonwebtoken");
 
-let loginAuth = function(req, res, next) {
-  jwt.verify(req.headers.token, process.env.SECRETKEYS, function(err, decoded) {
+let loginAuth = function (req, res, next) {
+  jwt.verify(req.headers.token, process.env.SECRETKEYS, function (
+    err,
+    decoded
+  ) {
     if (decoded) {
       next();
     } else {
-      res.send(err);
+      res.status(401).send(err);
     }
   });
 };
 
 module.exports = {
-  loginAuth
+  loginAuth,
 };
